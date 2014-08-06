4cf115481704eb0796929514fb7782d3095b7d64
Lars Lehtonen <lars.lehtonen@gmail.com>
2014-08-05 17:01:38 -0700

    changed directory structure
    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = "json26",
+    version = "0.0.1",
+    author = "python",
+    author_email = "lars.lehtonen@gmail.com",
+    description = ("Python 2.6 implmentation split out from standard library"),
+    license = "python software foundation license version 2",
+    keywords = "json",
+    url = "http://github.com/alrs/json26",
+    packages=['json26'],
+)
